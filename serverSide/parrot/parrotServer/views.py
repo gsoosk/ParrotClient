1f35273be4795ab76351f65958ea64e8dfb931e0
Farzad Habibi <farzadhabibii98@gmail.com>
2018-09-03 18:43:22 +0430

    add new mixin for creating

--- serverSide/parrot/parrotServer/views.py
+++ serverSide/parrot/parrotServer/views.py
@@ -1,18 +1,43 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import mixins
 from .models import Log
 from .models import Command
 from .serializers import LogSerilizer
 from .serializers import CommandSerilizer
 # Create your views here.
 
-class LogView(generics.ListCreateAPIView):
+class LogView(mixins.CreateModelMixin, generics.ListAPIView):
     queryset = Log.objects.all()
     serializer_class = LogSerilizer
 
-class CommandView(generics.ListCreateAPIView):
+    def perform_create(self, serializer):
+        serializer.save()
+        #You can acces name & familyName & id & phoneNumber
+        # with serializer.data['name'] & ...
+        # print(serializer.data['name'])
+        # print(serializer.data['familyName'])
+        # print(serializer.data['phoneNumber'])
+        # print(serializer.data['id'])
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class CommandView(mixins.CreateModelMixin, generics.ListAPIView):
     queryset = Command.objects.all()
     serializer_class = CommandSerilizer
+
+    def perform_create(self, serializer):
+        serializer.save()
+        # You can acces owner & commandName & id & param
+        # with serializer.data['name'] & ...
+        # print(serializer.data['owner'])
+        # print(serializer.data['commandName'])
+        # print(serializer.data['param'])
+        # print(serializer.data['id'])
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 class CommandsOfUserView(generics.ListAPIView):
     serializer_class = CommandSerilizer
